7ed5de34a002f126cb80d8ba458915d9281c0495
Ramil Vildanov <ramvildan.work@gmail.com>
2022-09-05 17:23:02 +0300

    Update ChatServer.java

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -23,22 +23,23 @@ public class ChatServer {
 
     private static void anotherClient(Socket client) {
         try {
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            Writer writer = new OutputStreamWriter(client.getOutputStream());
 
             String greetings = "Hello and welcome to MyChat! Please enter your name: ";
             writer.write(greetings+"\n");
             writer.flush();
-            String name = reader.readLine();
-            userName.add(name);
 
-            while (true) {
-                String line = reader.readLine();
-                if (line == null) break;
-                writer.write(userName.get(userName.indexOf(name)) + ": " + line + "\n");
-                writer.flush();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));){
+                String name = reader.readLine();
+                userName.add(name);
+
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) break;
+                    writer.write(userName.get(userName.lastIndexOf(name)) + ": " + line + "\n");
+                    writer.flush();
+                }
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
