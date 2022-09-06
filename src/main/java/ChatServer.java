4d1efbfe1a8c1359f9370167579766420dbb14b1
Ramil Vildanov <ramvildan.work@gmail.com>
2022-09-06 18:41:28 +0300

    UPD ChatServer.java (changed List)

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -7,17 +7,18 @@ import java.util.List;
 
 public class ChatServer {
 
-    private static List<String> userName = Collections.synchronizedList(new ArrayList<>());
+    private static String userName;
+    private static final List<Socket> allClients = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(4451)) {
             while (true) {
                 Socket client = server.accept();
+                allClients.add(client);
 
                 new Thread(() -> {
                     anotherClient(client);
                 }).start();
-
             }
         }
     }
@@ -27,14 +28,11 @@ public class ChatServer {
             Writer writer = new OutputStreamWriter(client.getOutputStream());
             BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-            String greetings = "Hello and welcome to MyChat! Please enter your name: ";
-            writer.write(greetings+"\n");
-            writer.flush();
+            greetingsToAll(client);
 
-            String name = reader.readLine();
-            userName.add(name);
+            userName = reader.readLine();
 
-            toAllClients(reader, name);
+            toAllClients(reader, userName);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,17 +40,24 @@ public class ChatServer {
             try {
                 client.close();
             } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private static void toAllClients(BufferedReader reader, String name) {
+    private static void greetingsToAll(Socket client) throws IOException {
+        Writer writeGreeting = new OutputStreamWriter(client.getOutputStream());
+        writeGreeting.write("Hello and welcome to MyChat! Please enter your name: " +"\n");
+        writeGreeting.flush();
+    }
+
+    private static void toAllClients(BufferedReader reader, String userName) {
         try {
             while (true) {
                 String line = reader.readLine();
                 if (line == null) break;
                 if (line.isBlank()) continue;
-                System.out.println(userName.get(userName.lastIndexOf(name)) + ": " + line);
+                System.out.println(userName + ": " + line);
             }
         } catch (IOException e) {
             e.printStackTrace();
