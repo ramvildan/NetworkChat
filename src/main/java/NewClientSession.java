4d9c6727313d4d6a09d71ee2feae426d681158b2
Ramil Vildanov <ramvildan.work@gmail.com>
2022-09-13 22:32:28 +0300

    UPD NewClient.java

--- src/main/java/NewClientSession.java
+++ src/main/java/NewClientSession.java
@@ -1,12 +1,29 @@
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
-public class NewClient {
+import static java.util.Objects.requireNonNull;
+
+public class NewClientSession {
+
+    private String login;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        requireNonNull(login, "login shouldn't be null");
+        if (this.login != null) {
+            throw new IllegalStateException("login change isn't allowed");
+        }
+        this.login = login;
+    }
 
     public static void main(String[] args) throws IOException {
         try (Socket socket = new Socket("localhost", 4451)) {
-            Writer writer = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true, StandardCharsets.UTF_8);
             new Thread(() -> {
                 socketReader(socket);
                 System.exit(0);
@@ -15,16 +32,13 @@ public class NewClient {
             consoleReader(writer);
         }
     }
-
-    private static void consoleReader(Writer writer) throws IOException {
+    private static void consoleReader(PrintWriter writer) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             String line = reader.readLine();
-            writer.write(line + "\n");
-            writer.flush();
+            writer.println(line);
         }
     }
-
     private static void socketReader(Socket socket) {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
