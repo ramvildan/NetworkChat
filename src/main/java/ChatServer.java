2fde58e2b184c78b89fefee19241033898279440
Ramil Vildanov <ramvildan.work@gmail.com>
2022-09-13 22:32:44 +0300

    UPD ChatServer.java

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,73 +1,92 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ChatServer {
 
-    private static String userName;
-    private static final List<Socket> allClientsList = Collections.synchronizedList(new ArrayList<>());
+    private static final List<NewClientSession> allSessionsList = Collections.synchronizedList(new ArrayList<>());
+    private final MassageList massages = new MassageList();
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(4451)) {
             while (true) {
                 Socket client = server.accept();
-                allClientsList.add(client);
 
-                new Thread(() -> {
-                    anotherClient(client);
-                }).start();
+                anotherClient(client);
             }
         }
     }
 
-    private static void anotherClient(Socket client) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    public static void anotherClient(Socket client) {
+        new Thread(() -> {
+            NewClientSession session = new NewClientSession();
+            BufferedReader reader = null;
+            PrintWriter writer = null;
 
-            greetingsToAll(writer);
-            userName = reader.readLine();
-
-            sendMessage(reader, client, userName);
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
             try {
-                client.close();
+                reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+                writer = new PrintWriter(client.getOutputStream(), true, StandardCharsets.UTF_8);
+
+                enterLogin(session, reader, writer);
+
+                allSessionsList.add(session);
+
+                sendMassages(session, reader, writer);
+
+
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                try {
+                    client.close();
+                    if (reader != null) {
+                        reader.close();
+                    }
+                    if (writer != null) {
+                        writer.close();
+                    }
+                    allSessionsList.remove(session);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        }).start();
     }
 
-    private static void greetingsToAll(Writer writer) throws IOException {
-        writer.write("Hello and welcome to MyChat! Please enter your name: " + "\n");
-        writer.flush();
-    }
-
-    private static void sendMessage(BufferedReader massageReader, Socket client, String userName) {
+    private static void sendMassages(NewClientSession session, BufferedReader reader, PrintWriter writer) {
         try {
             while (true) {
-                String message = massageReader.readLine();
+                String message = reader.readLine();
                 if (message == null) break;
                 if (message.isBlank()) continue;
 
-                for (Socket user : allClientsList) {
-                    if (!user.equals(client)) {
-                        Writer messageWriter = new OutputStreamWriter(user.getOutputStream());
-                        messageWriter.write(userName + ": " + message);
-                        messageWriter.write("\n");
-                        messageWriter.flush();
+                for (NewClientSession thisSession : allSessionsList) {
+                    if (thisSession.equals(session)) {
+                        writer.println(session.getLogin() + ": " + message);
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private static void enterLogin(NewClientSession session, BufferedReader reader, PrintWriter writer) throws IOException {
+        do {
+            writer.println("Hello and welcome to MyChat! Please enter your login:");
+            String clientResponse = reader.readLine();
+            if (clientResponse == null) {
+                throw new IOException();
+            }
+            if (!clientResponse.isBlank() && clientResponse.length() <= 10) {
+                session.setLogin(clientResponse);
+                break;
+            }
+
+            writer.println("This login is illegal, please try again...");
+        } while (true);
     }
 }
