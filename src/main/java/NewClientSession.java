9c7a40578ba7d5ea091c4a7151e887029a45106a
Ramil Vildanov <ramvildan.work@gmail.com>
2022-09-17 16:10:58 +0300

    UPD NewClientSession.java

--- src/main/java/NewClientSession.java
+++ src/main/java/NewClientSession.java
@@ -23,34 +23,34 @@ public class NewClientSession {
 
     public static void main(String[] args) throws IOException {
         try (Socket socket = new Socket("localhost", 4451)) {
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true, StandardCharsets.UTF_8);
-            new Thread(() -> {
-                socketReader(socket);
-                System.exit(0);
-            }).start();
-
-            consoleReader(writer);
+            socketReader(socket);
+            consoleReader(socket);
         }
     }
-    private static void consoleReader(PrintWriter writer) throws IOException {
+
+    private static void consoleReader(Socket socket) throws IOException {
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true, StandardCharsets.UTF_8);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             String line = reader.readLine();
             writer.println(line);
         }
     }
+
     private static void socketReader(Socket socket) {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-            while (true) {
-                String line = reader.readLine();
-                if (line == null) break;
-                if (line.isBlank()) continue;
-                System.out.println(line);
+        new Thread(() -> {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))){
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) break;
+                    if (line.isBlank()) continue;
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            System.exit(0);
+        }).start();
     }
 }
 
